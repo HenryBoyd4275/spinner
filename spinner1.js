ff148c77c676c6fe9f479586789a89333742b0fe
Henry Boyd <AmorphousCube@gmail.com>
2020-01-14 17:56:07 +0000

    created better spinner using loops. Does not handle more than 2 spins

--- spinner1.js
+++ spinner1.js
@@ -13,6 +13,5 @@ setTimeout(() => {
 
 setTimeout(() => {
   // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
+  process.stdout.write('\r\\   ');
 }, 700);
-// ... fill in the rest yourself ...
